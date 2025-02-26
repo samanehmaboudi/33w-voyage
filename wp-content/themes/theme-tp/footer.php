1c41ab31070b6137134a40cbf9c56b28e8842c0f
samanehmahboudi <e2495960@cmaisonneuve.qc.ca>
2025-02-26 15:56:27 -0500

    ajoute les modification etap2

--- wp-content/themes/theme-tp/footer.php
+++ wp-content/themes/theme-tp/footer.php
@@ -1,7 +1,25 @@
-</main>
-<footer>
-    <p>&copy; <?php echo date('Y'); ?> Mondo Voyages. Tous droits réservés.</p>
+</main> 
+<footer class="footer">
+    <div class="footer__contenu">
+        <div class="footer__texte">
+            <p>&copy; <?php echo date('Y'); ?> Mondo Voyages. Tous droits réservés.</p>
+        </div>
+        <nav class="footer__nav">
+            <ul class="footer__menu">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Destinations</a></li>
+                <li><a href="#">À propos</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="footer__reseaux">
+         <a href="#" class="footer__icon"><i class="fa-brands fa-facebook-f"></i></a>
+         <a href="#" class="footer__icon"><i class="fa-brands fa-instagram"></i></a>
+         <a href="#" class="footer__icon"><i class="fa-brands fa-x-twitter"></i></a>
+        </div>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
 </html>
+
--- wp-content/themes/theme-tp/header.php
+++ wp-content/themes/theme-tp/header.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
     <title><?php bloginfo('name'); ?> - <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
     <?php wp_head(); ?>
 </head>
