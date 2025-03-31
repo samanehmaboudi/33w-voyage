44bc46af8a38eb5c335139471a90f88bd32ee32c
samanehmahboudi <e2495960@cmaisonneuve.qc.ca>
2025-03-31 08:53:22 -0400

    ajout du formulaire de recherche + style SCSS dans le footer

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
         <div class="footer__texte">
             <p>&copy; <?php echo date('Y'); ?> Mondo Voyages. Tous droits réservés.</p>
         </div>
+
         <nav class="footer__nav">
             <ul class="footer__menu">
                 <li><a href="#">Accueil</a></li>
@@ -12,14 +13,27 @@
                 <li><a href="#">Contact</a></li>
             </ul>
         </nav>
+
         <div class="footer__reseaux">
-         <a href="#" class="footer__icon"><i class="fa-brands fa-facebook-f"></i></a>
-         <a href="#" class="footer__icon"><i class="fa-brands fa-instagram"></i></a>
-         <a href="#" class="footer__icon"><i class="fa-brands fa-x-twitter"></i></a>
+            <a href="#" class="footer__icon"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="#" class="footer__icon"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#" class="footer__icon"><i class="fa-brands fa-x-twitter"></i></a>
+        </div>
+
+        <div class="footer__recherche">
+            <?php if (function_exists('get_search_form')) : ?>
+                <?php get_search_form(); ?>
+            <?php else : ?>
+                <form action="<?php echo esc_url(home_url('/')); ?>" method="get">
+                    <input type="text" name="s" placeholder="Rechercher...">
+                    <button type="submit">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </form>
+            <?php endif; ?>
         </div>
     </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
 </html>
-
--- readme.md
+++ readme.md
@@ -1,8 +1,36 @@
-###Étape 1 du TP2
+# TP2 – Étape 1 : Champs personnalisés
+
+## Auteur
+**Samaneh Maboudi**  
+
+## Description
+Cette première étape du TP2 consiste à intégrer des **champs personnalisés** au site WordPress du club de voyage.
+
+Le site permet maintenant d'afficher, pour chaque destination :
+- **La température minimale et maximale** (grâce au plugin ACF)
+- Une **galerie d'images personnalisée** (si disponible)
+- Une **page de détail individuelle** (single.php)
+- Une **carte résumée** sur les pages de catégorie
+- Une page **404 personnalisée**
+
+## Fonctionnalités ajoutées
+- Champs ACF : `temperature_maximum`, `temperature_minimum`, `galerie`
+- Template `single.php` modifié avec affichage dynamique des champs
+- Gabarit `carte.php` intégrant les champs personnalisés
+- Fichier `404.php` avec message et recherche
+
+## Fichiers créés ou modifiés
+- `single.php`
+- `gabarits/carte.php`
+- `category.php`
+- `404.php`
+- `functions.php` (vérification support ACF)
 
 
-##33W - Introduction à un système de gestion de contenu
 
-##Développement du thème du TP2
+## Lien vers le site WHC :
 
-Les éléments seront ajoutés: Un custom logo Plusieurs template-part Une liste de catégorie pour les cartes Quelques champs personnalisés Le modèle catégory.php
+
+## Dépôt GitHub:  
+ https://github.com/samanehmaboudi/33w-voyage/tree/tp2-1
+
