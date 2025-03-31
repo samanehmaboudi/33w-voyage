52bde7bdfa9f26de992f50eb34fc1815a25ab469
samanehmahboudi <e2495960@cmaisonneuve.qc.ca>
2025-03-31 00:02:53 -0400

    feat: ajout de la section galerie et destinations populaires

--- wp-content/themes/theme-tp/front-page.php
+++ wp-content/themes/theme-tp/front-page.php
@@ -1,17 +1,26 @@
 <?php get_header(); ?>
+
+<!-- SECTION HERO avec image de fond -->
 <section class="hero" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/maldives.jpg');">
-    <div class="hero__contenu">
+    <div class="hero__contenu global">
         <h1 class="hero__titre">Voyagez Autrement avec Mondo Voyages !</h1>
         <p class="hero__description">
-            Découvrez des destinations uniques et inoubliables avec Mondo Voyages. Nous vous offrons des expériences authentiques, des voyages à couper le souffle et des aventures sur mesure. Partez à la découverte du monde avec nous et créez des souvenirs impérissables.
+            Découvrez des destinations uniques et inoubliables avec Mondo Voyages. Nous vous offrons des expériences authentiques, des voyages à couper le souffle et des aventures sur mesure.
         </p>
         <address class="hero__contact">
             <p>Email : <a href="mailto:info@cmaisonneuve.qc.ca">info@cmaisonneuve.qc.ca</a></p>
             <p>Téléphone : 514-254-7131</p>
         </address>
+        <div class="hero__sociaux">
+            <a href="#"><img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000" width="32" height="32" alt="Facebook"></a>
+            <a href="#"><img src="https://s2.svgbox.net/social.svg?ic=instagram&color=000" width="32" height="32" alt="Instagram"></a>
+            <a href="#"><img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000" width="32" height="32" alt="LinkedIn"></a>
+        </div>
         <button class="hero__bouton">S'INSCRIRE</button>
     </div>
 </section>
+
+<!-- FORMULAIRE -->
 <section class="formulaire">
     <form class="formulaire__contenu">
         <div class="form__group">
@@ -35,24 +44,56 @@
         </div>
     </form>
 </section>
+
+<!-- SECTION GALERIE -->
+<section class="galerie global">
+    <h2>Galerie</h2>
+    <div class="galerie__contenu">
+        <?php
+        $galerie_query = new WP_Query(array(
+            'category_name' => 'galerie',
+            'posts_per_page' => 6
+        ));
+
+        if ($galerie_query->have_posts()) :
+            while ($galerie_query->have_posts()) : $galerie_query->the_post(); ?>
+                <div class="galerie__item">
+                    <?php if (has_post_thumbnail()) : ?>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail('medium'); ?>
+                        </a>
+                    <?php endif; ?>
+                </div>
+        <?php endwhile;
+            wp_reset_postdata();
+        endif;
+        ?>
+    </div>
+</section>
+
+<!-- SECTION DESTINATIONS POPULAIRES -->
 <section class="populaire global">
-        <div class="global">
+    <h2>Destinations populaires</h2>
+    <div class="populaire__contenu">
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <?php if(in_category('Galerie')) {
-            the_content();
-        } else { ?>
-        <article>
-        <div class="carte carte--grande">
-  <div class="carte__contenu">
-    <h2 class="carte__titre"><?php the_title(); ?></h2>
-    <p class="carte__description"><?php echo wp_trim_words(get_the_excerpt(), 25, "..."); ?></p>
-  </div>
-</div>
-    </article>
-    <?php } ?>
-<?php endwhile; endif; ?>
- 
-        </div>
- 
-    </section>
+            <?php if (!in_category('galerie')) : ?>
+                <article class="carte carte--grande">
+                    <?php if (has_post_thumbnail()) : ?>
+                        <div class="carte__image">
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_post_thumbnail('custom-wide'); ?>
+                            </a>
+                        </div>
+                    <?php endif; ?>
+                    <div class="carte__contenu">
+                        <h3 class="carte__titre"><?php the_title(); ?></h3>
+                        <p class="carte__description"><?php echo wp_trim_words(get_the_excerpt(), 25, "..."); ?></p>
+                        <a href="<?php the_permalink(); ?>" class="btn">Voir la destination</a>
+                    </div>
+                </article>
+            <?php endif; ?>
+        <?php endwhile; endif; ?>
+    </div>
+</section>
+
 <?php get_footer(); ?>
