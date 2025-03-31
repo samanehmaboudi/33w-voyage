f448fd438f1aa3e76255c274703018ba6960c4d0
samanehmahboudi <e2495960@cmaisonneuve.qc.ca>
2025-03-31 00:03:09 -0400

    feat: entête avec logo, menu et recherche responsive

--- wp-content/themes/theme-tp/header.php
+++ wp-content/themes/theme-tp/header.php
@@ -5,16 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
+    <!-- Titre dynamique du site -->
     <title><?php bloginfo('name'); ?> - <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
+
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
     <header class="entete">
         <div class="entete__conteneur">
+
+            <!-- Logo personnalisé -->
             <figure class="entete__logo">
                 <?php
                 if (function_exists('the_custom_logo')) {
@@ -25,9 +30,11 @@
                 ?>
             </figure>
 
+            <!-- Burger menu pour mobile -->
             <input type="checkbox" class="entete__toggle" id="menu-checkbox">
             <label for="menu-checkbox" class="entete__burger">☰</label>
 
+            <!-- Menu principal -->
             <nav class="entete__menu">
                 <?php
                 wp_nav_menu(
@@ -41,12 +48,15 @@
                 ?>
             </nav>
 
-            <form class="entete__recherche" method="get" action="<?php echo home_url('/'); ?>">
-                <input type="text" name="s" placeholder="Rechercher" class="recherche__input">
+            <!-- Formulaire de recherche -->
+            <form class="entete__recherche" method="get" action="<?php echo esc_url(home_url('/')); ?>">
+                <input type="text" name="s" placeholder="Rechercher..." class="recherche__input">
                 <button type="submit" class="recherche__btn">
                     <img src="https://s2.svgbox.net/hero-outline.svg?ic=search&color=000" width="16" height="16" alt="Rechercher">
                 </button>
             </form>
+
         </div>
     </header>
+
     <main class="global">
